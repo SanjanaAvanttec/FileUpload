aba41f6aae6b515eb7ea1d1d79fea8fc27f3be95
SanjanaAvanttec <sanjana@avanttec.in>
2023-07-12 14:27:46 +0530

    sanajan src

---
+++ src/CHEMIST/Chemist.js
@@ -0,0 +1,62 @@
+import './chemist.css'
+import { useNavigate } from 'react-router-dom';
+import LogoutIcon from '@mui/icons-material/Logout';
+import EnquiryProcess from './Enquiry';
+import ChemistNavigationBar from './ChemistNavBar';
+
+function Chemist() {
+
+    const navigate = useNavigate();
+
+
+    return (
+        <> 
+            <div id="ChemistNavbar">
+
+                <ChemistNavigationBar/>
+
+                {/* <div className='row container'>
+                    <div className='col-sm-3'>
+
+                        <h1 className='display-3' >
+                           Chemist
+                        </h1>
+                    </div>
+
+                    <div className='col-sm-6 mt-4'>
+
+                        <a href='#Enquiry' id='setcolorenq'  className='h4'>
+                            
+                            ENQUIRY
+                        </a>
+                    </div>
+                    <div className='col-sm-3'>
+                    <button onClick={() => navigate("/login")} className='btn'>
+                                            <span>
+                                                LOGOUT <LogoutIcon/>
+                                           </span>
+                                           </button>
+                    </div>
+
+                </div> */}
+
+            </div>
+
+
+            <div id='Enquiry'className='ms-5 mt-5'>
+
+                <div id='setmt'>
+                <EnquiryProcess />
+              </div>
+
+            </div>
+            <div style={{
+                height:"100vh"
+            }}>
+
+            </div>
+        </>
+    )
+}
+
+export default Chemist;
